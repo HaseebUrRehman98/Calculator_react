9625ab236482c3d02faa5a2a6d0b397a6affca9d
seebi98 <36692369+seebi98@users.noreply.github.com>
2018-11-18 14:03:44 +0100

    second commit

--- App.js
+++ App.js
@@ -1,43 +1,60 @@
 import React from 'react';
-import { StyleSheet, Text, View,Button } from 'react-native';
+import { StyleSheet, Text, View,Button,TouchableOpacity } from 'react-native';
 
 export default class App extends React.Component {
+  constructor(){
+    super()
+    this.state={
+      resultText:"",
+    }
+  }
+  calculateResult(text){
+    return "0"
+  }
+  buttononPress(text){
+    if(text == '='){
+        return this.state.resultText
+    }
+   this.setState({
+     resultText:this.state.resultText+text
+   })
+  }
   render() {
-    let roper=[];
+    let rowOper=[];
     let rows=[];
-    let nums=[[1,2,3],[4,5,6],[7,8,9],[0,"=","%"]];
+    let nums=[[1,2,3],[4,5,6],[7,8,9],[0,'=',"."]];
     let oper=["+","/","-","*"];
     for(let i=0;i<4;i++)
     {
       let row=[];
       for(let j=0;j<3;j++)
       {
-        row.push(<Button style={styles.btn} title={""+nums[i][j]}/>);
+        row.push(<TouchableOpacity onPress={()=>this.buttononPress(nums[i][j])} style={styles.btn}><Text style={styles.buttonText}>{nums[i][j]}</Text></TouchableOpacity> );
       }
       rows.push(<View style={styles.row}>{row}</View>);
     }
     for(let i=0;i<4;i++)
     {
-      roper.push(<Button style={StyleSheet.btn} title={""+oper[i]}/>);
+      rowOper.push(<TouchableOpacity style={styles.btn}><Text style={styles.buttonTextOper}>{oper[i]}</Text></TouchableOpacity> );
     }
+
     return (
-      <View style={styles.container}>
+   <View style={styles.container}>
         <View style={styles.result}>
-       <Text style={styles.resultText}>2*2</Text>
+        <Text style={styles.resultText}>{this.state.resultText}</Text>
        </View>
        <View style={styles.calculation}>
        <Text style={styles.calculationText}>222</Text>
        </View>
       <View style={styles.buttons}>
-       <View styles={styles.numbers}>
-           {rows}
+         <View style={styles.numbers}>
+          {rows}
+         </View>
+         <View style={styles.operations}>
+          {rowOper}
+         </View>
       </View>
-      <View styles={styles.operations}>
-       {roper}
-       </View>
-      </View>
-       
-      </View>
+   </View>
     );
   }
 }
@@ -46,26 +63,32 @@ const styles = StyleSheet.create({
   container: {
     flex:1,
    },
+   buttonText:{
+   fontSize:30,
+   },
+   buttonTextOper:{
+    color:'white',
+    fontSize:30,
+   },
    btn:{
     color:'black',
     flex:1,
     justifyContent:'space-around',
-    justifyContent:'center',
     alignItems:'center',
-    alignSelf:'stretch',
+   
    },
   result:{
     flex:2,
     backgroundColor:'red',
     justifyContent:'center',
-    alignContent:'flex-end'
+    alignItems:'flex-end'
 
   },
   calculation:{
     flex:1,
     backgroundColor:'green',
     justifyContent:'center',
-    alignContent:'flex-end',
+    alignItems:'flex-end',
   },
   resultText:{
     fontSize:20,
@@ -80,18 +103,18 @@ const styles = StyleSheet.create({
     flexDirection:'row',
     flex:1,
     justifyContent:'space-around',
-    alignItems:'stretch',
-    alignSelf:'center',
+    alignItems:'center',
+   
     
   },
    buttons:{
-   flex:5,
+   flex:7,
    flexDirection:'row',
    justifyContent:'space-around',
   },
   numbers:{
-    flex:8,
-    backgroundColor:'red',
+    flex:3,
+    backgroundColor:'yellow',
     justifyContent:'space-around',
   },
   operations:{
