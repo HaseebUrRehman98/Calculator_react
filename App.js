ce89cb0e2a8bc6aafd76ffe9fb8adca72e3b853a
seebi98 <36692369+seebi98@users.noreply.github.com>
2018-12-13 16:47:42 +0100

    final

--- App.js
+++ App.js
@@ -9,6 +9,7 @@ export default class App extends React.Component {
       calculationText:'0',
       pointcounter:0,
       numberCounter:0,
+      pointDel:0
    }
   }
    buttononPress(text)
@@ -46,7 +47,7 @@ export default class App extends React.Component {
           pointcounter:1
         })
     }
-    if(this.state.calculationText == '0' && text!='.')
+    if(this.state.calculationText == '0' && text!='.') // change 0 to input number at start
     {
       this.setState({
         calculationText:text
@@ -58,16 +59,19 @@ export default class App extends React.Component {
       this.setState({
         calculationText:this.state.calculationText+''+text
       })
-     }
+    }
      return ""
-    
-    
-   
-  }
+    }
   
   isoperator(op)
   {
     if(op == "+" || op =="-" || op =="*" || op =="/" ) return true
+  }
+  adddata(text)
+  {
+    this.setState({
+      calculationText:this.state.calculationText+text
+    })
   }
   operate(operation)
   {
@@ -84,24 +88,54 @@ export default class App extends React.Component {
       }
         else
         {
-        let text=this.state.calculationText.split('');
-        text.pop();
-        this.setState({
-          calculationText:text.join(''),
-         
-        })
-      }
+          let text=this.state.calculationText.split('');
+        let del=text.pop();
+              if(del=='.')
+              {
+                this.setState({
+                  pointcounter:0
+                })
+              }
+              if(this.isoperator(del))
+              {
+                if(this.state.pointDel==1)
+                {
+                  this.setState({
+                    pointcounter:1,
+                    pointDel:0
+                  })
+                }
+              }
+              this.setState({
+                calculationText:text.join(''),
+              })
+        }
         break
       case "+":
       case "-":
       case "*":
       case "/":
-          if(this.isoperator((this.state.calculationText+'').split('')[this.state.calculationText.length-1])) return "";
+        if(this.isoperator((this.state.calculationText+'').split('')[this.state.calculationText.length-1]))
+        {
+         let prova=this.state.calculationText.split('');
+         prova.pop();
+         this.setState({
+            calculationText:prova.join() +operation  
+         })
+          return "";
+        }
         if((this.state.calculationText+'').split('')[this.state.calculationText.length-1]=='.') return "";
+        
            this.setState({
-            pointcounter:0,
             calculationText:this.state.calculationText+''+operation
         })
+        if(this.state.pointcounter==1)
+        {
+          this.setState({
+            pointcounter:0,
+            pointDel:1,
+          })
+        }
         break
       
     }
